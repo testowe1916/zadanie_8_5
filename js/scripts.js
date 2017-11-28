c6b96984c73d4cad35fc0ef0599dc54764001b26
Mariusz Sioch <mariusz.sioch@o2.pl>
2017-11-28 14:02:19 +0100

    Add js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var a = 3,
+    h = 4,
+    triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert(triangleArea)
+
+
+
